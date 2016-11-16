c428438790e89f2881118f251e1cbea905ada0d0
Lowell Scott <blaarg171@gmail.com>
2016-11-16 11:15:38 -0600

    Formatting, because I can't stand looking at tabs and inconsistent spacing :)

--- src/app.js
+++ src/app.js
@@ -20,8 +20,8 @@ var entityManager = new EntityManager();
 var combatController = new CombatController();
 
 
-var tilemap = new Tilemap({width: canvas.width, height: canvas.height}, 64, 64, tileset, {
-  onload: function() {
+var tilemap = new Tilemap({ width: canvas.width, height: canvas.height }, 64, 64, tileset, {
+  onload: function () {
     masterLoop(performance.now());
   }
 });
@@ -38,44 +38,44 @@ var input = {
 
 var randPos;                    //{x: , y: }
 var turnTimer = 0;
-var defaultTurnDelay = 400; 	  //Default turn between turns
+var defaultTurnDelay = 400;     //Default turn between turns
 var turnDelay = defaultTurnDelay; //current time between turns
-var autoTurn = false; 			  //If true, reduces time between turns and turns happen automatically
-var resetTimer = true; 			  //Take turn immediately on movement key press if true
+var autoTurn = false;           //If true, reduces time between turns and turns happen automatically
+var resetTimer = true;          //Take turn immediately on movement key press if true
 var loopCount = 0; //Temporary until camera movement is done
 
 randPos = tilemap.findOpenSpace();
-var player = new Player({x: randPos.x, y: randPos.y}, tilemap);
+var player = new Player({ x: randPos.x, y: randPos.y }, tilemap);
 
-var enemy = new Enemy({x:randPos.x+1, y:randPos.y+1}, tilemap); // temp - just spawn next to player
+var enemy = new Enemy({ x: randPos.x + 1, y: randPos.y + 1 }, tilemap); // temp - just spawn next to player
 
 window.player = player;
 
 entityManager.addEntity(player);
 entityManager.addEntity(enemy);
 
-tilemap.moveTo({x: randPos.x - 3 , y: randPos.y - 4});
+tilemap.moveTo({ x: randPos.x - 3, y: randPos.y - 4 });
 
-canvas.onclick = function(event){
+canvas.onclick = function (event) {
   var node = {
     x: parseInt(event.offsetX / 96),
     y: parseInt(event.offsetY / 96)
   }
 
-  turnDelay=defaultTurnDelay/2;
+  turnDelay = defaultTurnDelay / 2;
   autoTurn = true;
 
   var clickedWorldPos = Vector.add(tilemap.draw.origin, node);
-  if(enemy.position.x == clickedWorldPos.x && enemy.position.y == clickedWorldPos.y){
+  if (enemy.position.x == clickedWorldPos.x && enemy.position.y == clickedWorldPos.y) {
     console.log("clicked on enemy");
     var distance = Vector.subtract(player.position, enemy.position);
-    if(Math.abs(distance.x)<= player.combat.AttackRange && Math.abs(distance.y)<= player.combat.AttackRange){
+    if (Math.abs(distance.x) <= player.combat.AttackRange && Math.abs(distance.y) <= player.combat.AttackRange) {
       console.log("enemy within range");
       combatController.handleAttack(player.combat, enemy.combat);
     }
   } else {
-    player.walkPath(pathfinder.findPath(player.position, clickedWorldPos), function(){
-      turnDelay=defaultTurnDelay;
+    player.walkPath(pathfinder.findPath(player.position, clickedWorldPos), function () {
+      turnDelay = defaultTurnDelay;
       autoTurn = false;
     });
   }
@@ -85,96 +85,92 @@ canvas.onclick = function(event){
  * @function onkeydown
  * Handles keydown events
  */
-var position = {x: 0, y: 0};
-window.onkeydown = function(event) {
-   switch(event.key) {
-     case "ArrowUp":
-     case "w":
-       //position.y--;
-       input.up = true;
-	   if(resetTimer)
-	   {
-		   turnTimer = turnDelay;
-		   resetTimer = false;
-	   }
-	   event.preventDefault();
-       break;
-     case "ArrowDown":
-     case "s":
-       //position.y++;
-       input.down = true;
-	   if(resetTimer)
-	   {
-		   turnTimer = turnDelay;
-		   resetTimer = false;
-	   }
-	   event.preventDefault();
-       break;
-     case "ArrowLeft":
-     case "a":
-       //position.x--;
-       input.left = true;
-	   if(resetTimer)
-	   {
-		   turnTimer = turnDelay;
-		   resetTimer = false;
-	   }
-	   event.preventDefault();
-       break;
-     case "ArrowRight":
-     case "d":
-       //position.x++;
-       input.right = true;
-	   if(resetTimer)
-	   {
-		   turnTimer = turnDelay;
-		   resetTimer = false;
-	   }
-	   event.preventDefault();
-       break;
-	 case "Shift":
-		event.preventDefault();
-		turnDelay=defaultTurnDelay/2;
-		autoTurn = true;
-		break;
-   }
- }
-
- /**
-  * @function onkeyup
-  * Handles keyup events
-  */
-window.onkeyup = function(event) {
-    switch(event.key) {
-      case "ArrowUp":
-      case "w":
-        input.up = false;
-        break;
-      case "ArrowDown":
-      case "s":
-        input.down = false;
-        break;
-      case "ArrowLeft":
-      case "a":
-        input.left = false;
-        break;
-      case "ArrowRight":
-      case "d":
-        input.right = false;
-        break;
-	  case "Shift":
-        turnDelay=defaultTurnDelay;
-        autoTurn = false;
-		break;
-    }
-	if(!(input.left || input.right || input.up || input.down)) resetTimer = true;
+var position = { x: 0, y: 0 };
+window.onkeydown = function (event) {
+  switch (event.key) {
+    case "ArrowUp":
+    case "w":
+      //position.y--;
+      input.up = true;
+      if (resetTimer) {
+        turnTimer = turnDelay;
+        resetTimer = false;
+      }
+      event.preventDefault();
+      break;
+    case "ArrowDown":
+    case "s":
+      //position.y++;
+      input.down = true;
+      if (resetTimer) {
+        turnTimer = turnDelay;
+        resetTimer = false;
+      }
+      event.preventDefault();
+      break;
+    case "ArrowLeft":
+    case "a":
+      //position.x--;
+      input.left = true;
+      if (resetTimer) {
+        turnTimer = turnDelay;
+        resetTimer = false;
+      }
+      event.preventDefault();
+      break;
+    case "ArrowRight":
+    case "d":
+      //position.x++;
+      input.right = true;
+      if (resetTimer) {
+        turnTimer = turnDelay;
+        resetTimer = false;
+      }
+      event.preventDefault();
+      break;
+    case "Shift":
+      event.preventDefault();
+      turnDelay = defaultTurnDelay / 2;
+      autoTurn = true;
+      break;
   }
+}
+
+/**
+ * @function onkeyup
+ * Handles keyup events
+ */
+window.onkeyup = function (event) {
+  switch (event.key) {
+    case "ArrowUp":
+    case "w":
+      input.up = false;
+      break;
+    case "ArrowDown":
+    case "s":
+      input.down = false;
+      break;
+    case "ArrowLeft":
+    case "a":
+      input.left = false;
+      break;
+    case "ArrowRight":
+    case "d":
+      input.right = false;
+      break;
+    case "Shift":
+      turnDelay = defaultTurnDelay;
+      autoTurn = false;
+      break;
+  }
+  if (!(input.left || input.right || input.up || input.down)) resetTimer = true;
+}
 /**
  * @function masterLoop
  * Advances the game in sync with the refresh rate of the screen
  * @param {DOMHighResTimeStamp} timestamp the current time
  */
-var masterLoop = function(timestamp) {
+var masterLoop = function (timestamp) {
   game.loop(timestamp);
   window.requestAnimationFrame(masterLoop);
 }
@@ -189,14 +185,12 @@ var masterLoop = function(timestamp) {
  */
 function update(elapsedTime) {
 
-  if(input.left || input.right || input.up || input.down || autoTurn)
-  {
-	  turnTimer += elapsedTime;
-	  if(turnTimer >= turnDelay)
-	  {
-		turnTimer = 0;
-		processTurn();
-	  }
+  if (input.left || input.right || input.up || input.down || autoTurn) {
+    turnTimer += elapsedTime;
+    if (turnTimer >= turnDelay) {
+      turnTimer = 0;
+      processTurn();
+    }
   }
   entityManager.update(elapsedTime);
 }
@@ -220,6 +214,6 @@ function render(elapsedTime, ctx) {
   * @function processTurn
   * Proccesses one turn, updating the states of all entities.
   */
-function processTurn(){
-	entityManager.processTurn(input);
+function processTurn() {
+  entityManager.processTurn(input);
 }
--- src/player.js
+++ src/player.js
@@ -15,102 +15,99 @@ module.exports = exports = Player;
  * @param {postition} position object specifying an x and y
  */
 function Player(position, tilemap) {
-	this.state = "idle";
-	this.position = {x: position.x, y: position.y};
-	this.size = {width: 96, height: 96};
-	this.spritesheet  = new Image();
-	this.tilemap = tilemap;
-	this.spritesheet.src = './spritesheets/sprites.png';
-	this.type = "Player";
-	this.walk = [];
-	this.combat = new CombatStruct(this.type);
+    this.state = "idle";
+    this.position = { x: position.x, y: position.y };
+    this.size = { width: 96, height: 96 };
+    this.spritesheet = new Image();
+    this.tilemap = tilemap;
+    this.spritesheet.src = './spritesheets/sprites.png';
+    this.type = "Player";
+    this.walk = [];
+    this.combat = new CombatStruct(this.type);
 }
 
 /**
  * @function updates the player object
  * {DOMHighResTimeStamp} time the elapsed time since the last frame
  */
-Player.prototype.update = function(time) {
+Player.prototype.update = function (time) {
 
 }
 
 Player.prototype.walkPath = function (path, completion) {
-	path.shift();
-	this.walk = path;
-	this.walkCompletion = completion;
+    path.shift();
+    this.walk = path;
+    this.walkCompletion = completion;
 
-	if(this.walk.length == 0) completion();
+    if (this.walk.length == 0) completion();
 };
 
 /**
  *@function handles the players turn
  *{input} keyboard input given for this turn
  */
-Player.prototype.processTurn = function(input)
-{
+Player.prototype.processTurn = function (input) {
 
-	if(this.walk.length > 0){
-		// walk
-		this.position = {x:this.walk[0].x, y: this.walk[0].y};
-		this.walk.shift();
-		var self = this;
-		if(this.walk.length == 0) self.walkCompletion();
-	}else{
-		var change = {x: 0, y: 0};
-		if(input.up) change.y--;
-		else if(input.down) change.y++;
+    if (this.walk.length > 0) {
+        // walk
+        this.position = { x: this.walk[0].x, y: this.walk[0].y };
+        this.walk.shift();
+        var self = this;
+        if (this.walk.length == 0) self.walkCompletion();
+    } else {
+        var change = { x: 0, y: 0 };
+        if (input.up) change.y--;
+        else if (input.down) change.y++;
 
-		if (input.right) change.x++;
-		else if(input.left) change.x--;
+        if (input.right) change.x++;
+        else if (input.left) change.x--;
 
-		var position = Vector.add(this.position, change);
-		if(this.tilemap.isWall(position.x, position.y)) return;
+        var position = Vector.add(this.position, change);
+        if (this.tilemap.isWall(position.x, position.y)) return;
 
-		this.position = position;
-	}
+        this.position = position;
+    }
 
-	var screenCoor = Vector.subtract(this.position, this.tilemap.draw.origin);
+    var screenCoor = Vector.subtract(this.position, this.tilemap.draw.origin);
 
-	if(screenCoor.y < 1){
-		this.tilemap.moveBy({x: 0, y: -1});
-	}
+    if (screenCoor.y < 1) {
+        this.tilemap.moveBy({ x: 0, y: -1 });
+    }
 
-	if(screenCoor.y + 1 == this.tilemap.draw.size.height){
-		this.tilemap.moveBy({x: 0, y: 1});
-	}
+    if (screenCoor.y + 1 == this.tilemap.draw.size.height) {
+        this.tilemap.moveBy({ x: 0, y: 1 });
+    }
 
-	if(screenCoor.x < 1){
-		this.tilemap.moveBy({x: -1, y: 0});
-	}
+    if (screenCoor.x < 1) {
+        this.tilemap.moveBy({ x: -1, y: 0 });
+    }
 
-	if(screenCoor.x + 1 == this.tilemap.draw.size.width){
-		this.tilemap.moveBy({x: 1, y: 0});
-	}
+    if (screenCoor.x + 1 == this.tilemap.draw.size.width) {
+        this.tilemap.moveBy({ x: 1, y: 0 });
+    }
 
 }
 
-Player.prototype.collided = function(entity)
-{
+Player.prototype.collided = function (entity) {
 }
 
-Player.prototype.retain = function()
-{
-	return true;
+Player.prototype.retain = function () {
+    return true;
 }
 
 /**
  * @function renders the player into the provided context
  * {CanvasRenderingContext2D} ctx the context to render into
  */
-Player.prototype.render = function(elapsedTime, ctx) {
-	var position = Vector.subtract(this.position, this.tilemap.draw.origin);
+Player.prototype.render = function (elapsedTime, ctx) {
+    var position = Vector.subtract(this.position, this.tilemap.draw.origin);
 
-  ctx.drawImage(
-	this.spritesheet,
-	96, 480,
-	96, 96,
-	position.x*this.size.width, position.y*this.size.height,
-	96,96
-	);
+    ctx.drawImage(
+        this.spritesheet,
+        96, 480,
+        96, 96,
+        position.x * this.size.width, position.y * this.size.height,
+        96, 96
+    );
 
 }
