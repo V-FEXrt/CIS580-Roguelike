a107a61a84794a9257d5ba7ea0f1f8634dab1343
Lowell Scott <blaarg171@gmail.com>
2016-11-21 23:31:15 -0600

    Added some general enemy classes. I don't like the naming of enemies vs player classes though.

--- src/combat_struct.js
+++ src/combat_struct.js
@@ -40,19 +40,44 @@ function CombatStruct(aType) {
             this.stamina = 100;
             this.someOtherPowerup = 50;
             this.weaponLevel = 1;
-            this.armorLevel = 3;
+            this.armorLevel = 1;
             this.attackType = "Melee";
             this.attackRange = 1;
-            this.senseRange = 15;
+            this.senseRange = 15; // This might be too high, what if we keep the camera centered..?
             break;
 
         case "EnemyRanged":
+            this.health = 10;
+            this.stamina = 100;
+            this.someOtherPowerup = 50;
+            this.weaponLevel = 1;
+            this.armorLevel = 3;
+            this.attackType = "Ranged";
+            this.attackRange = 5;
+            this.senseRange = 15;
             break;
 
         case "Captain":
+            this.health = 25;
+            this.stamina = 100;
+            this.someOtherPowerup = 50;
+            this.weaponLevel = 1;
+            this.armorLevel = 5;
+            this.attackType = "Melee";
+            this.attackRange = 1;
+            this.senseRange = 20;
             break;
 
         case "Shaman":
+            this.health = 10;
+            this.stamina = 100;
+            this.someOtherPowerup = 50;
+            this.weaponLevel = 1;
+            this.armorLevel = 1;
+            this.attackType = "Magic";
+            this.attackRange = 10;
+            this.senseRange = 15;
             break;
     }
 }
+
