cb5d4e2289d55c36fbcb2a99f97b55e75a966db6
Georgeda <georgeda@ksu.edu>
2016-12-01 20:28:38 -0600

    no message

--- bundle.js
+++ bundle.js
@@ -1155,7 +1155,7 @@ GUI.prototype.update = function (time) {
 			this.titleDirection = 1;
 		}
 		
-		this.titleY += this.titleDirection * time/150;
+		this.titleY += this.titleDirection/10;
 	}
 }
 
