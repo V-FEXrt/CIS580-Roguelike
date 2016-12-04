f8f4f4b3bee958185a3e3f193bf09f7dc676f2f8
Ashley Coleman <ashley78@ksu.edu>
2016-12-04 17:18:17 -0600

    Different sprites for different weapons

--- src/weapon.js
+++ src/weapon.js
@@ -9,6 +9,15 @@ function Weapon(aName, aLevel) {
     this.name = aName;
     this.level = aLevel;
     this.shouldRetain = true;
+    this.spriteIdx = 0;
+    this.spritePositions = [
+      {x: 75, y: 75},  // 0 - Magic Staff
+      {x: 225, y: 75}, // 1 - Bow
+      {x: 300, y: 75}, // 2 - Mace
+      {x: 375, y: 75}, // 3 - Knife/Sword
+      {x: 450, y: 75}, // 4 - Axe
+      {x: 675, y: 75}  // 5 - Another Magic Staff
+    ];
 
     switch (aName) {
         // Melee
@@ -20,6 +29,7 @@ function Weapon(aName, aLevel) {
             this.hitBonus = 0;
             this.attackEffect = "";
             this.properties = "+1 Min Damage";
+            this.spriteIdx = 3;
             break;
 
         case "Morning Star":
@@ -30,6 +40,7 @@ function Weapon(aName, aLevel) {
             this.hitBonus = 2;
             this.attackEffect = "";
             this.properties = "+2 to Hit";
+            this.spriteIdx = 2;
             break;
 
         case "Halberd":
@@ -40,6 +51,7 @@ function Weapon(aName, aLevel) {
             this.hitBonus = 0;
             this.attackEffect = "";
             this.properties = "+1 Range";
+            this.spriteIdx = 4;
             break;
 
         case "Battleaxe":
@@ -50,6 +62,7 @@ function Weapon(aName, aLevel) {
             this.hitBonus = 1;
             this.attackEffect = "";
             this.properties = "+3 Min Damage, +1 Crit Chance";
+            this.spriteIdx = 4;
             break;
 
         case "Claw":
@@ -60,6 +73,7 @@ function Weapon(aName, aLevel) {
             this.hitBonus = 0;
             this.attackEffect = "";
             this.properties = "+1 Min Damage";
+            this.spriteIdx = 3;
             break;
 
         // Ranged
@@ -71,6 +85,7 @@ function Weapon(aName, aLevel) {
             this.hitBonus = 3;
             this.attackEffect = "";
             this.properties = "+1 Range, +3 to Hit";
+            this.spriteIdx = 1;
             break;
 
         case "Broadhead":
@@ -81,6 +96,7 @@ function Weapon(aName, aLevel) {
             this.hitBonus = 0;
             this.attackEffect = "";
             this.properties = "+1 Min Damage";
+            this.spriteIdx = 1;
             break;
 
         case "Poison-Tipped":
@@ -91,6 +107,7 @@ function Weapon(aName, aLevel) {
             this.hitBonus = 0;
             this.attackEffect = "Poisoned";
             this.properties = "50% Poison Chance";
+            this.spriteIdx = 1;
             break;
 
         case "Heavy Bolts":
@@ -101,6 +118,7 @@ function Weapon(aName, aLevel) {
             this.hitBonus = 0;
             this.attackEffect = "";
             this.properties = "+3 Min Damage, -2 Range";
+            this.spriteIdx = 1;
             break;
 
         // Spells
@@ -112,6 +130,7 @@ function Weapon(aName, aLevel) {
             this.hitBonus = 255;
             this.attackEffect = "";
             this.properties = "Never Misses";
+            this.spriteIdx = 0;
             break;
 
         case "Fireball":
@@ -122,6 +141,7 @@ function Weapon(aName, aLevel) {
             this.hitBonus = 0;
             this.attackEffect = "Burned";
             this.properties = "Explodes on Contact, 50% Burn Chance";
+            this.spriteIdx = 5;
             break;
 
         case "Frostbolt":
@@ -132,6 +152,7 @@ function Weapon(aName, aLevel) {
             this.hitBonus = 0;
             this.attackEffect = "Frozen";
             this.properties = "50% Freeze Chance";
+            this.spriteIdx = 5;
             break;
 
         case "Eldritch Blast":
@@ -142,6 +163,7 @@ function Weapon(aName, aLevel) {
             this.hitBonus = -2;
             this.attackEffect = "";
             this.properties = "-2 to Hit";
+            this.spriteIdx = 0;
             break;
     }
 
@@ -179,5 +201,6 @@ Weapon.prototype.update = function () {
 
 Weapon.prototype.render = function (time, ctx) {
   var position = window.tilemap.toScreenCoords(this.position);
-  ctx.drawImage(this.spritesheet, 375, 75, 75, 75, (position.x * this.size.width), (position.y * this.size.height) + this.currY, 96, 96);
+  var spriteSource = this.spritePositions[this.spriteIdx];
+  ctx.drawImage(this.spritesheet, spriteSource.x, spriteSource.y, 75, 75, (position.x * this.size.width), (position.y * this.size.height) + this.currY, 96, 96);
 }
